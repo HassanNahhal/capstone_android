2c44b24c8d76126ea07da389eeaa5358cd69e2d8
Hassan Nahhal <hassannahhal@Hassans-MacBook-Pro.local>
2016-05-28 23:44:50 -0400

    Add about and remove FloatingActionButton

--- Receipt_Keeper/app/src/main/java/com/example/hassannahhal/receipt_keeper/SignUpUsingGoogleActivity.java
+++ Receipt_Keeper/app/src/main/java/com/example/hassannahhal/receipt_keeper/SignUpUsingGoogleActivity.java
@@ -1,29 +1,30 @@
 package com.example.hassannahhal.receipt_keeper;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 public class SignUpUsingGoogleActivity extends AppCompatActivity {
 
+    // ========================================================================================
+    // ============================   About this Class       ==================================
+
+    /* This class will be the class that runs after the user press Continue using Google button.
+    */
+    // ========================================================================================
+
+
+    // ========================================================================================
+    // ========================    Declaring attributes     ===================================
+
+
+    // ========================================================================================
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_using_google);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
 }
