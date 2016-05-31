ce101351df10e28c912fa8ead1a7f8992cd989b2
Hassan Nahhal <hassannahhal@Hassans-MacBook-Pro.local>
2016-05-31 01:33:20 -0400

    Add Google sign
    Now the user can sign in using google, this version only shows user
    data such as name,email,id, and photo url

--- Receipt_Keeper/app/src/main/java/com/example/hassannahhal/receipt_keeper/MainActivity.java
+++ Receipt_Keeper/app/src/main/java/com/example/hassannahhal/receipt_keeper/MainActivity.java
@@ -2,16 +2,29 @@ package com.example.hassannahhal.receipt_keeper;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class MainActivity extends AppCompatActivity implements
+        GoogleApiClient.OnConnectionFailedListener, View.OnClickListener {
 
 
+    //
     // ========================================================================================
     // ============================   About this Class       ==================================
 
@@ -24,12 +37,27 @@ public class MainActivity extends AppCompatActivity {
     // ========================================================================================
     // ========================    Declaring attributes     ===================================
 
-    Button facebookLoginButton;
-    Button googleLoginButton;
+    private Button facebookLoginButton;
+    private Button googleLoginButton;
+    private GoogleApiClient mGoogleApiClient;
+
+    private TextView mStatusTextView;
+
+
+    private static final String TAG = "SignInActivity";
+    private static final int RC_SIGN_IN = 9001;
 
 
     // ========================================================================================
 
+
+    // ============================       Google Login       ==================================
+
+    /*
+    * We might need to get background server's OAuth 2.0 to authenticate with a backend server
+    * check
+    * https://developers.google.com/identity/sign-in/android/start-integrating#add_google_play_services*/
+    // ========================================================================================
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,23 +67,106 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
 
+        mStatusTextView = (TextView) findViewById(R.id.status);
+
+
+        findViewById(R.id.sign_in_button).setOnClickListener(this);
+
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(
+                GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+
+
+        // Build a GoogleApiClient with access to the Google Sign-In API and the
+        // options specified by gso.
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+
+        // Customize sign-in button. The sign-in button can be displayed in
+        // multiple sizes and color schemes. It can also be contextually
+        // rendered based on the requested scopes. For example. a red button may
+        // be displayed when Google+ scopes are requested, but a white button
+        // may be displayed when only basic profile is requested. Try adding the
+        // Scopes.PLUS_LOGIN scope to the GoogleSignInOptions to see the
+        // difference.
+        SignInButton signInButton = (SignInButton) findViewById(R.id.sign_in_button);
+        signInButton.setSize(SignInButton.SIZE_STANDARD);
+        signInButton.setScopes(gso.getScopeArray());
+
+
     }
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.sign_in_button:
+                signIn();
+                break;
+            // ...
+        }
+    }
+
+
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+        }
+    }
+
+    private void handleSignInResult(GoogleSignInResult result) {
+        Log.d(TAG, "handleSignInResult:" + result.isSuccess());
+        if (result.isSuccess()) {
+            // Signed in successfully, show authenticated UI.
+            GoogleSignInAccount acct = result.getSignInAccount();
+
+            mStatusTextView.setText(getString(R.string.signed_in_fmt)
+                    + "\n" + acct.getDisplayName()
+                    + "\n" + acct.getEmail()
+                    + "\n" + acct.getId()
+                    + "\n" + acct.getPhotoUrl()
+            );
+
+            //updateUI(true);
+
+        } else {
+            // Signed out, show unauthenticated UI.
+            //updateUI(false);
+            Log.d(TAG, "handleSignInResult:" + result.isSuccess());
+
+        }
+    }
+
+    /*private void updateUI(boolean signedIn) {
+        if (signedIn) {
+            findViewById(R.id.sign_in_button).setVisibility(View.GONE);
+            findViewById(R.id.sign_out_and_disconnect).setVisibility(View.VISIBLE);
+        } else {
+            mStatusTextView.setText(R.string.signed_out);
+
+            findViewById(R.id.sign_in_button).setVisibility(View.VISIBLE);
+            findViewById(R.id.sign_out_and_disconnect).setVisibility(View.GONE);
+        }
+    }*/
 
     public void signUpUsingFacebook(View view) {
         Intent facebookIntent = new Intent(this, SignUpUsingFacebookActivity.class);
         facebookLoginButton = (Button) findViewById(R.id.facebookLoginButton);
         startActivity(facebookIntent);
     }
-
-
-    public void signUpUsingGoogle(View view) {
-        Intent googleIntent = new Intent(this, SignUpUsingGoogleActivity.class);
-        googleLoginButton = (Button) findViewById(R.id.googleLoginButton);
-        startActivity(googleIntent);
-    }
-
-
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -77,5 +188,12 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        // An unresolvable error has occurred and Google APIs (including Sign-In) will not
+        // be available.
+        Log.d(TAG, "onConnectionFailed:" + connectionResult);
     }
 }
