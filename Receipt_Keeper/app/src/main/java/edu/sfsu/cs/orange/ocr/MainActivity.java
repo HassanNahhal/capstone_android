740faf511b4c0891611ad6297ca409e5ac929101
Hassan Nahhal <hassannahhal@Hassans-MacBook-Pro.local>
2016-06-06 01:49:12 -0400

    Create mainActivity

---
+++ Receipt_Keeper/app/src/main/java/edu/sfsu/cs/orange/ocr/MainActivity.java
@@ -0,0 +1,53 @@
+package edu.sfsu.cs.orange.ocr;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+
+import static android.Manifest.permission.CAMERA;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private static final String PREF_IS_FIRST_RUN = "firstRun";
+    private SharedPreferences prefs;
+
+
+    private static final String[] PERMS_TAKE_PICTURE = {
+            CAMERA,
+            WRITE_EXTERNAL_STORAGE
+    };
+
+    private static final int RESULT_PERMS_INITIAL = 1339;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        prefs = PreferenceManager.getDefaultSharedPreferences(this);
+
+
+        if (isFirstRun()) {
+            ActivityCompat.requestPermissions(this, PERMS_TAKE_PICTURE,
+                    RESULT_PERMS_INITIAL);
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+// TODO
+    }
+
+    private boolean isFirstRun() {
+        boolean result = prefs.getBoolean(PREF_IS_FIRST_RUN, true);
+        if (result) {
+            prefs.edit().putBoolean(PREF_IS_FIRST_RUN, false).apply();
+        }
+        return (result);
+    }
+}
