571bd9c38743000589fead6c712f164cf0a5f02c
Hassan Nahhal <hassannahhal@Hassans-MacBook-Pro.local>
2016-06-17 05:14:23 -0400

    Add Receipt class
    Include constructor, setters and getters

---
+++ Receipt_Keeper/app/src/main/java/edu/sfsu/cs/orange/ocr/Receipt.java
@@ -0,0 +1,80 @@
+package edu.sfsu.cs.orange.ocr;
+
+import java.util.Date;
+
+/**
+ * Created by hassannahhal on 2016-06-14.
+ */
+public class Receipt {
+
+    private int id;
+    private int customerId;
+    private int storeId;
+    private int categroyId;
+    private String comment;
+    private Date date;
+
+
+    public Receipt() {
+    }
+
+    public Receipt(int id, int customerId, int storeId, int categroyId, String comment, Date date) {
+        this.id = id;
+        this.customerId = customerId;
+        this.storeId = storeId;
+        this.categroyId = categroyId;
+        this.comment = comment;
+        this.date = date;
+    }
+
+
+    // Attribute Getters
+    public int getId() {
+        return id;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public int getStoreId() {
+        return storeId;
+    }
+
+    public int getCategroyId() {
+        return categroyId;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    // Attribute Setters
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public void setStoreId(int storeId) {
+        this.storeId = storeId;
+    }
+
+    public void setCategroyId(int categroyId) {
+        this.categroyId = categroyId;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
