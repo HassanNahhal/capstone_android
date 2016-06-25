b881a12cf8ac7c15bcfbad04c4ae2358685ae98a
Changho Choi <cch.choi@gmail.com>
2016-06-25 16:44:30 -0400

    Modify pkg

---
+++ Receipt_Keeper/app/src/main/java/com/conestogac/receipt_keeper/Tag.java
@@ -0,0 +1,38 @@
+package com.conestogac.receipt_keeper;
+
+/**
+ * Created by hassannahhal on 2016-06-14.
+ */
+public class Tag {
+
+    private int tagId;
+    private int tagName;
+
+    public Tag() {
+    }
+
+    public Tag(int tagId, int tagName) {
+        this.tagId = tagId;
+        this.tagName = tagName;
+    }
+
+
+    // Attribute Getters
+    public int getTagId() {
+        return tagId;
+    }
+
+    public int getTagName() {
+        return tagName;
+    }
+
+
+    // Attribute Setters
+    public void setTagId(int tagId) {
+        this.tagId = tagId;
+    }
+
+    public void setTagName(int tagName) {
+        this.tagName = tagName;
+    }
+}
---
+++ Receipt_Keeper/app/src/main/java/com/conestogac/receipt_keeper/ocr/MultiSelectionDialog.java
@@ -0,0 +1,68 @@
+package com.conestogac.receipt_keeper;
+
+/**
+ * Created by Nicholas on 2016-06-15.
+ */
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.content.DialogInterface.OnMultiChoiceClickListener;
+import android.view.Gravity;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MultiSelectionDialog extends DialogFragment {
+    public String result = "";
+    ArrayList<String> choices = new ArrayList<String>();
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        final String[] items = getResources().getStringArray(R.array.save_items);
+        final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        builder.setTitle("Field Choices").setMultiChoiceItems(R.array.save_items, null, new DialogInterface.OnMultiChoiceClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+                if (isChecked) {
+                    choices.add(items[which]);
+
+                } else if (choices.contains(items[which])) {
+                    choices.remove(items[which]);
+                }
+            }
+        }).setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String storeName = getArguments().getString("Store Name");
+                String amount = getArguments().getString("Amount");
+                String selected = getArguments().getString("selected");
+                //Toast.makeText(getActivity(), "You chose:" + choices.get(0), Toast.LENGTH_SHORT).show();
+                result = choices.get(0);
+
+                if (result.equals("Store Name")) {
+                    storeName = selected;
+                }
+                if (result.equals("Amount")) {
+                    amount = selected;
+                }
+                String toastString = "Store Name:" + storeName + "\n" + "Amount:" + amount;
+
+                Toast toast = Toast.makeText(getActivity(), toastString, Toast.LENGTH_SHORT);
+
+                toast.show();
+            }
+        });
+
+        // return super.onCreateDialog(savedInstanceState);
+        return builder.create();
+    }
+
+    public String getResult() {
+        return result;
+    }
+}
